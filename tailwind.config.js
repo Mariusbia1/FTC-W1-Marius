4e0c6750a507e149465afc7b94860d7a5954de62
Marius_GBK <mariusbiaou75@gmail.comgit config --global user.email mariusbiaou75@linux.org>
2025-08-22 21:19:30 +0100

    SaaS Futurist App

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js,css}"],
+   theme: {
+     extend: {
+      colors:{
+        nav: "#0B081C",
+        text: "#F2F4F8",
+        h1:"#ECECEC"
+      },
+     },
+   },
+   plugins: [],
+ }
